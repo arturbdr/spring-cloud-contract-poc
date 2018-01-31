35d9085c2e6d0488e527127fea8ae1c5a029d2c2
Artur Drummond <arturbdr@gmail.com>
2018-01-30 23:46:26 -0200

    Client side project commit

---
+++ client-side/src/main/java/com/learning/spring/clientside/ClientSideApplication.java
@@ -0,0 +1,12 @@
+package com.learning.spring.clientside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSideApplication.class, args);
+	}
+}
---
+++ client-side/src/test/java/com/learning/spring/clientside/ClientSideApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learning.spring.clientside;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientSideApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
