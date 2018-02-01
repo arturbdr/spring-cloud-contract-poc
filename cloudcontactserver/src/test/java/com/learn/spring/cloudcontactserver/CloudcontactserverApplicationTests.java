be181f83670d9472cddbd4dfe76095a09507f1f5
Artur Drummond <arturbdr@gmail.com>
2018-01-31 23:30:51 -0200

    - Cloud contract first commit

---
+++ cloudcontactserver/src/test/java/com/learn/spring/cloudcontactserver/CloudcontactserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.spring.cloudcontactserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudcontactserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
