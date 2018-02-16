0667a4c4e468930691224dec1fbf2ebafd7d12b5
Artur Drummond <arturbdr@gmail.com>
2018-02-15 23:50:44 -0200

    - Start implementing spring cloud contract

---
+++ clientside/src/test/java/com/learn/client/clientside/gateway/http/to/Address.java
@@ -0,0 +1,10 @@
+package com.learn.client.clientside.gateway.http.to;
+
+public class Address {
+
+    "address" : {
+        "street" : "Boulevard",
+                "number" : 123,
+                "zipcode" : "04330123"
+    }
+}
---
+++ clientside/src/test/java/com/learn/client/clientside/gateway/http/to/Person.java
@@ -0,0 +1,13 @@
+package com.learn.client.clientside.gateway.http.to;
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+    private String name;
+    private int age;
+    private Address address;
+
+
+}
